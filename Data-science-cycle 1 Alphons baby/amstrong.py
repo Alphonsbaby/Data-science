c09b6f939c46b39ba5dfa1482516c1a19079c704
Alphonsbaby <74651127+Alphonsbaby@users.noreply.github.com>
2021-11-29 11:25:40 +0530

    Update amstrong.py

--- Data-science-cycle 1 Alphons baby/amstrong.py
+++ Data-science-cycle 1 Alphons baby/amstrong.py
@@ -1,12 +1,9 @@
-lower = int(input("Enter lower range: "))
-upper = int(input("Enter upper range: "))
-
-for num in range(lower, upper + 1):
-    sum = 0
-    temp = num
-    while temp > 0:
-        digit = temp % 10
-        sum += digit ** 3
-        temp //= 10
-        if num == sum:
-            print(num)
+for num in range(1,1001):
+  temp=num
+  sum=0
+  while temp>0:
+      digit=temp%10
+      sum=sum+digit**3
+      temp=temp//10
+  if sum==num:
+      print (num)
