9f87c42091a0a8d2efb2bfb000ea82cee14aa2fa
Alphonsbaby <74651127+Alphonsbaby@users.noreply.github.com>
2021-12-06 12:40:04 +0530

    Add files via upload

---
+++ Data-science-cycle 2 Alphons baby/c2 part 2_4.py
@@ -0,0 +1,10 @@
+import numpy as np
+m1 = np.random.randint(20, size=(2, 2))
+print(m1)
+m2 = np.random.randint(20, size=(2, 2))
+print(m2)
+m3 = np.random.randint(20, size=(2, 2))
+print(m3)
+print("multiplication of the 3 matrices")
+m4 = np.dot(m1,m2,m3) 
+print(m4) 
