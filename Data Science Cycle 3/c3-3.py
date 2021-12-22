e16b0b860e123fb2cee89e87016de4b29a6b0e71
Alphonsbaby <74651127+Alphonsbaby@users.noreply.github.com>
2021-12-22 11:26:32 +0530

    Update c3-3.py

--- Data Science Cycle 3/c3-3.py
+++ Data Science Cycle 3/c3-3.py
@@ -5,19 +5,17 @@ import numpy as np
 plt.title("Sales Data")
 plt.xlabel("Months of Year")
 plt.ylabel("Sale of Food")
-x = np.array([173,153,195,147,120,144,148,109,174,130,172,131])
-y = np.array([173,153,195,147,120,144,148,109,174,130,172,131])
-plt.scatter(x,y, color = 'hotpink')
+x = np.array(['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'])
 
-x = np.array([185,185,126,134,196,153,112,133,200,145,167,110])
-y = np.array([185,185,126,134,196,153,112,133,200,145,167,110])
-plt.scatter(x, y, color = 'yellow')
+y1 = np.array([173,153,195,147,120,144,148,109,174,130,172,131])
+plt.scatter(x,y1, color = 'hotpink')
 
 
-x = np.array([189,189,105,112,173,109,151,197,174,145,177,161])
-y = np.array([189,189,105,112,173,109,151,197,174,145,177,161])
-plt.scatter(x, y, color = 'blue')
+y2 = np.array([185,185,126,134,196,153,112,133,200,145,167,110])
+plt.scatter(x, y2, color = 'yellow')
 
 
+
+y3 = np.array([189,189,105,112,173,109,151,197,174,145,177,161])
+plt.scatter(x, y3, color = 'blue')
 plt.show()
-
